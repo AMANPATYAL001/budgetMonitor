22ec88afdecfb4a41c864ed55bece0d8ba197828
Aman Patyal <amanpatyal79@gmail.com>
2022-09-01 21:51:11 +0530

    news api changed

--- news.js
+++ news.js
@@ -1,5 +1,5 @@
 async function getData() {
-  const response = await fetch('https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=0254aebaf29b4f92aef1df34a0b016e6')
+  const response = await fetch('https://newsdata.io/api/1/news?apikey=pub_10814437b5992ca5a23ca4017c49e75903ff0&q=finance&country=in&language=en&category=business')
   const data = await response.json();
   return data;
   // .then((data) => { return data }).catch((error) => {
@@ -8,36 +8,36 @@ async function getData() {
 }
 async function main2() {
   let data = await getData()
-  console.log(data)
+  // console.log(data)
   if (data == 'FAIL')
     return
-  data = data['articles']
+  data = data['results']
   let i = 0
   while (i < data.length) {
     $('#news-offcanvas').append(`
           <div class="row my-2">
             <div class="col-sm-6 m-0">
-              <div class="card">
-                <img src=${data[i].urlToImage} class="card-img-top" alt="No Image">
+              <div class="card border-success">
+                <img src=${data[i].image_url} class="card-img-top" alt="No Image">
                 <div class="card-body">
                   <h6 class="card-title">${data[i].title}</h6>
+                  <a href="${data[i].link}" target=”_blank” class="stretched-link"></a>
                 </div>
-                <div class="card-footer text-muted">
-                <p>
-                    ${data[i].publishedAt}</p>
-                </div>
+                <p class="card-text"><small class="text-muted">
+                    ${data[i].pubDate}</small>
+                </p>
               </div>
             </div>
             <div class="col-sm-6 m-0">
-              <div class="card">
-                <img src=${data[i + 1].urlToImage} class="card-img-top" alt="No Image">
+              <div class="card border-success">
+                <img src=${data[i + 1].image_url} class="card-img-top" alt="No Image">
                 <div class="card-body">
                   <h6 class="card-title">${data[i + 1].title}</h6>
+                  <a href="${data[i + 1].link}" target=”_blank” class="stretched-link"></a>
                 </div>
-                <div class="card-footer text-muted">
-                <p>
-                    ${data[i + 1].publishedAt}</p>
-                </div>
+                <p class="card-text"><small class="text-muted">
+                    ${data[i + 1].pubDate}</small>
+                </p>                
               </div>
             </div>
           </div>`);
@@ -45,8 +45,12 @@ async function main2() {
   }
   $('#newsGIF').hide()
 }
+let newsUpdated = false;
+
 window.addEventListener("DOMContentLoaded", $('#newsBtn').click(() => {
-  
-  main2()
-  
+  if (!newsUpdated) {
+    main2()
+    newsUpdated = true
+  }
+
 }))
