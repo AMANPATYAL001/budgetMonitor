250fe643050257fd7c8efb801e4da7af5649b78b
Aman Patyal <amanpatyal79@gmail.com>
2022-12-03 21:17:18 +0530

    added scrolling effect

--- app.js
+++ app.js
@@ -1179,3 +1179,22 @@ for (var i = 0; i < radioButtons.length; i++) {
 
 // $('#themeChange').click(()=>{
 // })
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 200;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+    } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+  
